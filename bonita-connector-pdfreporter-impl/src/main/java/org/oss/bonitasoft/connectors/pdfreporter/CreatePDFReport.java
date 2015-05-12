5b068a07068b04e01dd257e04584baba1e48f667
magnus <mkarlsson@open-software-solutions.ch>
2015-05-12 13:39:58 +0200

    Apache License v2.0

--- bonita-connector-pdfreporter-impl/src/main/java/org/oss/bonitasoft/connectors/pdfreporter/CreatePDFReport.java
+++ bonita-connector-pdfreporter-impl/src/main/java/org/oss/bonitasoft/connectors/pdfreporter/CreatePDFReport.java
@@ -1,9 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2015 Open Software Solutions GmbH.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v3.0
+ * are made available under the terms of the Apache License v2.0
  * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl-3.0.html
  *
  * Contributors:
  *     Open Software Solutions GmbH
